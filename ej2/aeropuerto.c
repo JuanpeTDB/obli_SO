7304912323a88538f26c13c4241b53505e27017b
JuanpeTDB <albanelljuanpedro@gmail.com>
2025-10-29 15:27:25 -0300

    Ej2

--- ej2/aeropuerto.c
+++ ej2/aeropuerto.c
@@ -6,83 +6,40 @@
 #include <time.h>
 #include <stdint.h>
 
-int rem_pas = 100;
-int rem_ofi = 15;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-// devuelve: 1 = OFICINISTA, 0 = PASAJERO, -1 = no queda nadie
-int next_accion(void) {
-    int total = rem_pas + rem_ofi;
-    if (total == 0) return -1;
+int rand_1_5(void){ unsigned limit = RAND_MAX - (RAND_MAX + 1u) % 5; unsigned r; do { r = (unsigned)rand(); } while (r > limit); return 1 + (r % 5); }
+int rand_1_3(void){ unsigned limit = RAND_MAX - (RAND_MAX + 1u) % 3; unsigned r; do { r = (unsigned)rand(); } while (r > limit); return 1 + (r % 3); }
 
-    int r = rand() % total;         
-    if (r < rem_ofi) {              
-        rem_ofi--;
-        return 1;
-    } else {                        
-        rem_pas--;
-        return 0;
+void* pasajero(void* arg){
+    int id = (int)(intptr_t)arg;
+    pthread_mutex_lock(&mutex);
+    printf("Pasajero %d esta mirando el cartel...\n", id);
+    sleep(rand_1_3());
+    pthread_mutex_unlock(&mutex);
+    return NULL;
+}
+
+void* oficinista(void* arg){
+    int id = (int)(intptr_t)arg;
+    for (int i = 1; i <= 3; i++) {
+        pthread_mutex_lock(&mutex);
+        printf("Oficinista %d esta cambiando el cartel en iteracion %d...\n", id, i);
+        sleep(rand_1_5());
+        pthread_mutex_unlock(&mutex);
     }
+    return NULL;
 }
 
-
-// Funcion que genera un numero random entre 1 y 5, hecha con chatgpt
-int rand_1_5(void) {
-    unsigned limit = RAND_MAX - (RAND_MAX + 1u) % 5;
-    unsigned r;
-    do { r = (unsigned)rand(); } while (r > limit);
-    return 1 + (r % 5);
-}
-
-// Funcion que genera un numero random entre 1 y 3, hecha con chatgpt
-int rand_1_3(void) {
-    unsigned limit = RAND_MAX - (RAND_MAX + 1u) % 3;
-    unsigned r;
-    do { r = (unsigned)rand(); } while (r > limit);
-    return 1 + (r % 3);
-}
-
-void* pasajero(void* arg){ 
-    int tiempo = rand_1_3();
-    int id = (int)(intptr_t)arg;
-    printf("Pasajero %d esta mirando el cartel... \n", id);
-    sleep(tiempo);
-    pthread_exit(0);
-}
-
-void* oficinista(void* arg){  
-    int tiempo = rand_1_5();
-    int id = (int)(intptr_t)arg;
-    printf("Oficinista %d esta cambiando el cartel... \n", id);
-    sleep(tiempo);
-    pthread_exit(0);
-
-}
-
-int main(){
+int main(void){
     srand((unsigned)time(NULL));
-    int quienVa = 0;
-    int pas=1;
-    int ofi=1;
-    int modOfi=1;
-    while ((quienVa = next_accion()) != -1)
-    {
-        if (quienVa == 1)
-        {
-            pthread_t hilo;
-            pthread_create(&hilo, NULL, oficinista, (void*)(intptr_t)ofi);
-            pthread_join(hilo, NULL);
-            modOfi++;
-            if (modOfi > 3)
-            {
-                modOfi=1;
-                ofi++;
-            }
-        } else if(quienVa == 0){ 
-            pthread_t hilo2;
-            pthread_create(&hilo2, NULL, pasajero, (void*)(intptr_t)pas);
-            pthread_join(hilo2, NULL);
-            pas++;
-        }
-    }
+    pthread_t ofi[5], pas[100];
+
+    for (int i = 0; i < 5; i++) pthread_create(&ofi[i], NULL, oficinista, (void*)(intptr_t)(i + 1));
+    for (int i = 0; i < 100; i++) pthread_create(&pas[i], NULL, pasajero, (void*)(intptr_t)(i + 1));
+
+    for (int i = 0; i < 5; i++) pthread_join(ofi[i], NULL);
+    for (int i = 0; i < 100; i++) pthread_join(pas[i], NULL);
+
     return 0;
 }
